ff40a798d1301450d3037e7051d4261c80506b5a
Samuel Coelho <samuelfcmc@gmail.com>
2016-06-12 16:38:52 +0100

    Improved build process and added support for sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const electron = require('electron-prebuilt');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
+const sass = require('gulp-sass');
 
 const tscConfig = require('./tsconfig.json');
 
@@ -12,11 +13,15 @@ const config = {
     build: {
         path: 'build',
         app: 'build/app',
-        lib: 'build/lib'   
+        lib: 'build/lib',
+		style: 'build/style'   
     },
     src: {
+		scripts: 'app/**/*.ts',
         app: 'app',
-        html: 'app/**/*.html'
+        html: 'app/**/*.html',
+		style: 'app/main.scss',
+		styles: 'app/**/*.scss'
     }
 };
 
@@ -25,10 +30,10 @@ gulp.task('run', ['build'], function () {
   childProcess.spawn(electron, ['--debug=5858','.'], { stdio: 'inherit' }); 
 }); 
 
-gulp.task('compile', function () {
+gulp.task('compile:script', function () {
   return gulp
     .src([
-		'app/**/*.ts',
+		config.src.scripts,
 		'typings/browser.d.ts'
 	])
     .pipe(sourcemaps.init())
@@ -67,8 +72,40 @@ gulp.task('copy:assets', function() {
       'index.html',
       config.src.html
     ])
-    .pipe(gulp.dest(config.build.path))
+    .pipe(gulp.dest(config.build.path));
 });
 
-gulp.task('build', ['copy:libs', 'copy:assets', 'copy:systemjs', 'compile']);
-gulp.task('default', ['run']);
+// Reload
+gulp.task('reload:scripts', ['compile:script'], function() {
+
+});
+
+gulp.task('reload:html', ['copy:assets'], function() {
+
+});
+
+gulp.task('reload:style', ['compile:style'], function() {
+
+});
+
+// SASS
+gulp.task('compile:style', function() {
+	return gulp.src(config.src.style)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(config.build.style));
+});
+
+// Watch
+gulp.task('watch', ['run'], function() {
+	gulp.watch(config.src.scripts, ['reload:scripts']);
+	gulp.watch([config.src.html, 'index.html'], ['reload:html']);
+	gulp.watch([config.src.styles], ['reload:style']);
+});
+
+gulp.task('build', ['copy:libs', 
+	'copy:assets', 'copy:systemjs', 
+	'compile:script', 'compile:style']);
+
+gulp.task('dev', ['watch']);
+
+gulp.task('default', ['dev']);
